1c7dd1141f1c9172f423ae336a7e00e17683086a
imranrasulzade <imranrasulzade5@gmail.com>
2025-07-16 20:53:57 +0400

    first commit

---
+++ src/main/java/com/example/appkubernetesdemo/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.appkubernetesdemo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+@RequiredArgsConstructor
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+
+        return "Hello World!";
+    }
+
+    @GetMapping("/salam")
+    public String sayHello2() {
+        return "Salam!";
+    }
+}
